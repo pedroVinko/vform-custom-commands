4adaaa9e80738f11ca383528ef20b8ffba93feb4
Pedro <pedro@vinkodigital.com>
2022-04-20 13:08:32 -0400

    ♻️ upgrade download backup command

--- download-backup.sh
+++ download-backup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-heroku pg:backups:capture -a vform-production
+read -p 'Heroku app name: ' heroku_app
+
+heroku pg:backups:capture -a $heroku_app
 
 read -p 'Heroku backup ID: ' backup_id
 
-heroku pg:backups:url $backup_id -a vform-production
+heroku pg:backups:url $backup_id -a $heroku_app
